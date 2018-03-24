b48bb5e5310a7617527862b647071eccf3d88e40
Ben Posey <brp76@pitt.edu>
2018-03-24 17:26:49 -0400

    Gradle Integration

---
+++ src/main/java/shared/MType.java
@@ -0,0 +1,20 @@
+/*
+
+MType class for message types
+
+
+access them by saying MType.AUTH;
+
+and so on...
+*/
+package shared;
+
+public class MType {
+	public static final int AUTH = 0;
+	public static final int OCC = 1;
+	public static final int SPEED = 2;
+	public static final int DOORS = 3;
+	public static final int TEMP = 4;
+	public static final int BRAKES = 5;
+	public static final int LIGHTS = 6;
+}
