9420967530ee70579636c188594041eaea4cfefc
devoncallanan <devoncallanan@gmail.com>
2018-03-28 10:58:51 -0400

    changed trainwreck driver

--- src/main/java/shared/Trainwreck.java
+++ src/main/java/shared/Trainwreck.java
@@ -3,5 +3,19 @@ package shared;
 public class Trainwreck {
 	public static void main(String[] args) {
 		System.out.println("Gradle Test");
+		
+		MessageQueue messagequeue = new MessageQueue();
+		
+		CTC ctc = new CTC(messagequeue);
+		TrackController trackctl = new TrackController(messagequeue);
+		TrackModel trackmodel = new TrackModel(messagequeue);
+		TrainModel trainmodel = new TrainModel(messagequeue);
+		TrainController trainctl = new TrainController(messagequeue);
+		
+		ctc.run();
+		trackctl.run();
+		trainmodel.run();
+		trainmodel.run();
+		trainctl.run();
 	}
 }
