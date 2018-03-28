a8c181e6748a6d366acb1f591848895bda3025bb
devoncallanan <devoncallanan@gmail.com>
2018-03-28 01:53:03 -0400

    added updated trackmodel files and set up class for message testing

--- src/main/java/modules/trackmodel/TrackModel.java
+++ src/main/java/modules/trackmodel/TrackModel.java
@@ -1,4 +1,43 @@
 package modules.trackmodel;
+
+
 public class TrackModel {
 	
+	MessageQueue m
+
+	public TrackModel(MessageQueue m) {
+		this.m = m;
+	}
+	
+	public run() {
+		
+		//check messages
+		
+		java.util.Stack mailbox = m.recieve(MDest.TrMd);
+		
+		while (!mailbox.empty()) {
+			Message mail = mailbox.pop();
+			
+			switch (mail.type()) {
+				case MType.AUTH:
+					System.out.println(mail.dataI());
+					mailbox.send(mail, MDest.TrMd);
+					break;
+				case MType.SPEED:
+					System.out.println(mail.dataI());
+					mailbox.send(mail, MDest.TrMd);
+					break;					
+			}
+				
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+		
+		//check messages
+		
+		
+	}
 }
