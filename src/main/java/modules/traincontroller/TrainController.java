af96ff0d9d048adc1f407179f02d9d5b526945f2
jed121 <31700404+jed121@users.noreply.github.com>
2018-03-28 10:47:29 -0400

    Train controller skeleton with a message receiver

--- src/main/java/modules/traincontroller/TrainController.java
+++ src/main/java/modules/traincontroller/TrainController.java
@@ -1,4 +1,41 @@
-package modules.traincontroller;
+
+import java.util.Stack;
+
 public class TrainController {
+	
+	double suggestedSpeed;
+	double velocityFeedback;
+	MessageQueue messages;
+	Stack<Message> mymail;
+	Message currentM;
+	
+	
+	public TrainController(MessageQueue messages) {
+		this.messages = messages;
+	}
+	
+	
+	public void run() {
+		
+		mymail = messages.receive(4);
+		
+		while(!mymail.isEmpty()) {
+			currentM = mymail.pop();
+			
+			switch(currentM.type()){
+				case 2:
+					velocityFeedback = currentM.dataD();
+					break;
+				default:
+					break;
+			}
+			System.out.println("vF = " + velocityFeedback);
+		}
+		//Send lights on
+		messages.send(new Message(MDest.TrCtl, 1, MType.LIGHTS), MDest.TrMd);
+		messages.send(new Message(MDest.TrCtl, 0, MType.LIGHTS), MDest.TrMd);
+		messages.send(new Message(MDest.TrCtl, 1, MType.LIGHTS), MDest.TrMd);
+		
+	}
 	
 }
