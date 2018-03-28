720cc1579501d64aaabb2a0ad40d81dd0a9c3e14
Ben Posey <brp76@pitt.edu>
2018-03-28 11:01:04 -0400

    CTC Message Test

--- src/main/java/modules/ctcoffice/CTCOffice.java
+++ src/main/java/modules/ctcoffice/CTCOffice.java
@@ -1,4 +1,35 @@
 package modules.ctcoffice;
+import java.util.*;
+import shared.*;
+
 public class CTCOffice {
-	
+	private int speed;
+	public MessageQueue mq = new MessageQueue();
+	private Stack<Message> messages;
+	private Message m;
+
+	public CTCOffice(MessageQueue i){
+	  mq = i;
+	  speed = 1186;
+	}
+
+	public void run(){
+	  mReceive();
+	  mSend();
+	}
+
+	public void mReceive(){
+	  messages = mq.receive(MDest.CTC);
+	  while(!messages.isEmpty()){
+	       m = messages.pop();
+	       if(m.type() == MType.SPEED){
+	            speed = m.dataI();
+	       }
+	  }
+	}
+
+	public void mSend(){
+       m = new Message(MDest.CTC, speed, Mtype.SPEED);
+       mq.send(m, MDest.TcCtl);
+	}
 }
