57fc6ac094010bfabcb852e05914a79538525537
Jennifer Dudek <jed121@pitt.edu>
2018-04-08 17:56:58 -0400

    moved Nimbus to constructor method

--- src/main/java/modules/traincontroller/TrainController.java
+++ src/main/java/modules/traincontroller/TrainController.java
@@ -57,6 +57,18 @@ public class TrainController {
 		power3 = new Power();
 		velocity = new Velocity();
         mode = true;
+		
+		//Initialize my GUI
+		try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(TestingUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
 		java.awt.EventQueue.invokeLater(() -> {
             new TestingUI(this).setVisible(true);
         });
