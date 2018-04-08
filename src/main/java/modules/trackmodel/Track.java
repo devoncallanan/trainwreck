171928485e062553a86d1ea2439faad691acef79
devoncallanan <devoncallanan@gmail.com>
2018-04-08 17:33:19 -0400

    added Train class

--- src/main/java/modules/trackmodel/Track.java
+++ src/main/java/modules/trackmodel/Track.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package modules.trainmodel;
+package modules.trackmodel;
 
 import shared.*;
 import java.util.regex.Pattern;
---
+++ src/main/java/shared/Train.java
@@ -0,0 +1,27 @@
+/*train thing that tells me where it is and i move it and stuff*/
+
+package modules.trackmodel;
+
+import shared.*;
+import java.util.regex.Pattern;
+
+
+public class Train{
+	public int location;					//Block number
+	public int frontNode;					//Node number that the train is traveling toward
+	public double speed;					//Current speed m/s
+	public double distanceIn;				//Distance into the block km
+	public int id;							//train ID
+	private static double DELTA_T = .001;		//quantum for calculations
+	
+	public Train(int id, int location) {
+		this.locaiton = location;
+		this.id = id;
+		this.distanceIn = 0;
+	}
+	
+	public double move() {
+		distanceIn += distanceIn = (DELTA_T*speed);
+	}
+	
+}
