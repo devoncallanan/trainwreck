229062e29d4179a190094c7dd8f472a5bfc415c7
devoncallanan <devoncallanan@gmail.com>
2018-04-09 20:02:24 -0400

    changed the package info

--- src/main/java/modules/trainmodel/TrainModelMain.java
+++ src/main/java/modules/trainmodel/TrainModelMain.java
@@ -3,8 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package my.trainmodel;
-//import shared.*;
+package modules.trainmodel;
+import shared.*;
 import java.util.*;
 /**
  *
