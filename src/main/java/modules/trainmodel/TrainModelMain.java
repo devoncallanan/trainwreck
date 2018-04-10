7fb24d28004c5a5a8dce66c71271b38962c96dc8
bek63 <35611707+bek63@users.noreply.github.com>
2018-04-09 21:44:33 -0400

    Update TrainModelMain.java

--- src/main/java/modules/trainmodel/TrainModelMain.java
+++ src/main/java/modules/trainmodel/TrainModelMain.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package my.trainmodel;
+package modules.trainmodel;
 import shared.*;
 import java.util.*;
 /**
