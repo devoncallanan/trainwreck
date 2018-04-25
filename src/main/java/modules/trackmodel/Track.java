eb6b68ba41d2ff456a61ecd2266011027b25fb72
devoncallanan <devoncallanan@gmail.com>
2018-04-24 21:47:09 -0400

    added multi occ

--- src/main/java/modules/trackmodel/Track.java
+++ src/main/java/modules/trackmodel/Track.java
@@ -71,10 +71,20 @@ public static double MS_TO_KMH = 3600.0/10000.0;
             int to = scan.nextInt();
             int branch = scan.nextInt();
 			int switchloc = scan.nextInt();
-			String beacon = null;
-			if (scan.nextInt() == 1) {
-				beacon = "Cool Station";
+			String beacon = scan.next();
+			if (beacon.equals("null") ) {
+				beacon = null;
 			}
+			String station = scan.next();
+			if (station.equals("null") ) {
+				station = null;
+			}			
+			int underground = scan.nextInt();
+			boolean crossing = scan.nextBoolean();
+			String extra = scan.next();
+			if (extra.equals("null") ) {
+				extra = null;
+			}				
 			String infrastructure = scan.next();
 			
             Block b = new Block(to, from, section, number, infrastructure, grade, bLength, limit, branch, switchloc, beacon); 
@@ -191,6 +201,14 @@ public static double MS_TO_KMH = 3600.0/10000.0;
     public Block getBlock(int index) {
         return blocks[index];
     }
+	
+	public boolean[] getOccupancies() {
+		boolean[] occs = new boolean[blocks.length];
+		for (int i = 0; i < blocks.length; i++) {
+			occs[i] = blocks[i].occupied;
+		}
+		return occs;
+	}
     
     public int getSize() {
         return items;
--- src/main/java/modules/trackmodel/Train.java
+++ src/main/java/modules/trackmodel/Train.java
@@ -14,18 +14,32 @@ public static double MS_TO_KMH = 3600.0/10000.0;
 	public int backNode;					//Node number that the train is traveling from
 	public double speed;					//Current speed m/s
 	public double distanceIn;				//Distance into the block m
+	public double distanceInTail;
+	public double tailOverflow;
+	public java.util.ArrayList<Block> blocks;
 	public int id;							//train ID
+	public double init;
 	private static double DELTA_T = .01;		//quantum for calculations
 	
 	public Train(int id, int location, int node) {
 		this.location = location;
 		this.id = id;
 		this.distanceIn = 0;
+		this.distanceInTail = 0;
+		this.init = 0;
 		this.backNode = node;
+		this.blocks = new java.util.ArrayList<Block>();
 	}
 	
 	public double move() {
 		distanceIn += (DELTA_T*speed)*KMH_TO_MS;
+		if (init < 64.4) {
+			init += (DELTA_T*speed)*KMH_TO_MS;
+		}
+		else {
+			distanceInTail += (DELTA_T*speed)*KMH_TO_MS;
+			tailOverflow = distanceInTail - blocks.get(0).length;
+		}
 		return distanceIn;
 	}
 	
